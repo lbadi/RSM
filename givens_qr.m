01cb1b59aa6a781271c439bed45e67ecf629854f
lbadi <leonelbadi@hotmail.com>
2015-10-08 11:25:48 -0300

    Rotaciones de givens corregidas

--- givens_qr.m
+++ givens_qr.m
@@ -8,18 +8,18 @@ function [Q,R] = givens_qr(A)
       if (R(l,k) == 0)
         c = 1;
         s = 0;
-      elseif norm(R(l,k)) < norm(R(k,k))
+      elseif abs(R(l,k)) < abs(R(k,k))
         t = R(l,k) / R(k,k);
-        c = 1 / sqrt(1+pow2(t));
+        c = 1 / sqrt(1+t^2);
         s = c * t;
       else
         z = R(k,k) / R(l,k);
-        s = 1/ sqrt(1 + pow2(z));
+        s = 1/ sqrt(1 + z^2);
         c = s*z;
       end
       G = [c,s; -s,c];
       R([k,l] , k:n) = G * R([k,l], k : n);
-      Q([k,l] , k:n) = G * Q([k,l], k : n);
+      Q([k,l] , 1:n) = G * Q([k,l], 1:n);
     end
   end
 Q = Q';
