45d0aaf906e2ee2fc00ee25dd09fe7eb78f99541
Francisco Depascuali <FranDepascuali@Franciscos-MacBook-Pro.local>
2015-10-09 19:26:21 -0300

    minor change in quadratic

--- cuadratic_eq.m
+++ cuadratic_eq.m
@@ -1,4 +1,5 @@
 function [x1, x2] = cuadratic_eq(a, b, c)
-  x2 = (-b + sqrt(b^2 - 4*a*c))/(2*a);
-  x1 = (-b - sqrt(b^2 - 4*a*c))/(2*a);
+  disc = sqrt(b^2 - 4*a*c)
+  x2 = (-b + disc)/(2*a);
+  x1 = (-b - disc)/(2*a);
 end
