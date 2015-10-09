4ffb99162e09e4977cec5d03e8dc5499caf28521
lbadi <leonelbadi@hotmail.com>
2015-10-09 11:09:14 -0300

    Double shifting Francis

--- qr_eig.m
+++ qr_eig.m
@@ -16,12 +16,13 @@ function [values, firstA] = qr_eig(n, nzr)
       A = R * Q;
       it = it+1;
     until(norm(A(n-actual_value, n-actual_value-1)) < error || it == max_iterations);
-    if(norm(A(n-actual_value, n-actual_value-1)) < error)
+    if (abs(A(n-actual_value,n-actual_value-1)) < error*(abs(A(n-actual_value-1,n-actual_value-1))+abs(A(n-actual_value,n-actual_value)))) %Shif
       values(n-actual_value,1) = A(n-actual_value,n-actual_value);
 
       A = A(1:n-actual_value-1, 1:n-actual_value-1);
       actual_value = actual_value+1;
-    else
+
+    elseif (abs(A(n-actual_value-1,n-actual_value-2)) < error*(abs(A(n-actual_value-1,n-actual_value-1))+abs(A(n-actual_value-2,n-actual_value-2)))) %Double-Shif
       a = A(n-actual_value-1,n-actual_value-1);
       b = A(n-actual_value-1,n-actual_value);
       c = A(n-actual_value,n-actual_value-1);
@@ -33,13 +34,21 @@ function [values, firstA] = qr_eig(n, nzr)
       A = A(1:n-actual_value-2, 1:n-actual_value-2);
       actual_value = actual_value + 2;
     end
-  until(actual_value >= n-1);
-  if(actual_value == n-1)
+  until(actual_value >= n-2);
+  if(actual_value == n-2)
+      a = A(n-actual_value-1,n-actual_value-1);
+      b = A(n-actual_value-1,n-actual_value);
+      c = A(n-actual_value,n-actual_value-1);
+      d = A(n-actual_value,n-actual_value);
+      [x1, x2] = cuadratic_eq(1, -a-d, a*d - c*b);
+      values(n-actual_value-1,1) = x1;
+      values(n-actual_value,1) = x2;
+      A = A(1:n-actual_value-2, 1:n-actual_value-2);
+  elseif(actual_value == n-1)
     values(1) = A(1,1);
   end
   % values = sort(values, 'descend');
   toc;
 end
-
 
 
