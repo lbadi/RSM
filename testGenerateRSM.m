0dd07e437c1282b69d6a367197284afa79784370
FranDepascuali <FranDepascuali@Franciscos-MacBook-Pro.local>
2015-10-10 20:26:27 -0300

    continuando informe

--- testGenerateRSM.m
+++ testGenerateRSM.m
@@ -1,6 +1,6 @@
 function [graph,time] = testGenerateRSM()
 	j = 1;
-	for i=100:500:1500
+	for i=100:200:1500
 		initTime = time();
 		% Los nzn son el 20% de la dimension de la matriz
 		generateRSM(i,i*0.20);
@@ -9,6 +9,12 @@ function [graph,time] = testGenerateRSM()
 		y(j) = totalTime;
 		j++;
 	end
+	set (0, "defaultaxesfontname", "Helvetica")
+	hold on;
 	graph = plot(x,y)
+	title ("RSM");
+	xlabel ("Dimension");
+	ylabel ("Tiempo(s)");
 	time = y;
+	print -djpg image.jpg
 end
